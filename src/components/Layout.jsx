60e9bf1bd00b4c5dc8e5bac81017ad227d4aa5da
RN984 <rin994610@gmail.com>
2025-05-03 00:44:33 +0900

    updates

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,16 +7,18 @@ import '../index.css';
 export default function Layout() {
   return (
     <>
-      <header className="header">
-        <Header />
+       <header className="header">
+<Header />
       </header>
-
+      
       <div className="mainGrid">
         <nav className="navColumn">
           <NavColumn />
         </nav>
 
         <div className="mainContent">
+         
+        
           <Outlet />
         </div>
       </div>
